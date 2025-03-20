d96cc0171451fae41ec17327c2c1fc3fb4aff079
Ravikumar Jha <jharavi153@gmail.com>
2025-03-20 18:41:14 +0530

    Add Polyfills

---
+++ This.Polyfills.js
@@ -0,0 +1,63 @@
+const person = {
+  name : "Ravikumar",
+  lastName : "Jha" , 
+}
+
+function fullname (age , city){
+  console.log(`Name is ${this.name} & lastName is ${this.lastName} & age is ${age} & live in ${city} `)
+}
+
+fullname.call(person , 27 , "Mumbai");
+
+// Call PolyFills 
+ 
+Function.prototype.myCall = function(context , ...args){
+  if(typeof this !== "function"){
+    throw new console.error(this + "It is not Callable");
+  }
+
+  context.fn = this;
+  context.fn(...args);
+
+}
+
+fullname.myCall(person, 26 , "Delhi")
+
+
+//Apply Polyfills
+
+Function.prototype.myApply = function(context , args = []){
+  if(typeof this !== "function"){
+    throw new console.error(this + "It is not Callable");
+  }
+
+  if(!Array.isArray(args)){
+    throw new TypeError("CreateListFromArray like called on non-object");
+  }
+
+  context.fn = this;
+  context.fn(...args);
+
+}
+
+fullname.myApply(person, [22, "Surat"])
+
+//Bind polyfills 
+
+// Bind polyfill
+Function.prototype.myBind = function(context, ...args) {
+  if (typeof this !== "function") {
+    throw new TypeError(this + " can not be bound");
+  }
+
+  const fn = this;
+  return function(...newArgs) {
+    return fn.apply(context, [...args, ...newArgs]);
+  };
+};
+
+
+const newFunc = fullname.myBind(person, 21, "Banglore");
+
+newFunc(); 
+
