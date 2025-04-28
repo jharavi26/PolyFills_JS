ac2fa302ca0473f7035acb1555131cd8ef9c6f7c
Ravikumar Jha <jharavi153@gmail.com>
2025-04-28 20:27:02 +0530

    Add Question

---
+++ Array in-built Method.js
@@ -0,0 +1,6 @@
+const arr = [10,11,5,7,82];
+arr.push(31);                             //Add Element last in array 
+arr.pop();                                 //Remove last elemnt in array  
+arr.unshift(26);                           //Add Elemnt in starting  
+arr.shift();                               //Remove Element from starting                              
+console.log(arr);
--- Practice.js
+++ Practice.js
@@ -1,1 +1,16 @@
+const arr = [2,1,4,5,6,1];
+function sort(arr){
+for(let i=0; i<arr.length; i++){
+  for(let j=0; j<arr.length-i-1; j++){
+  if(arr[j]> arr[j+1]){
+    [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+  }
+}
+}
+return arr;
+}
 
+console.log(sort(arr));
+
+const result = arr.sort((a,b)=>b-a);
+console.log(result);
