ff6f1bf9e1fbea4fdd00565d988e3111f259fb78
Ravikumar Jha <jharavi153@gmail.com>
2025-04-29 20:04:58 +0530

    Array Method

--- Array in-built Method.js
+++ Array in-built Method.js
@@ -1,6 +1,11 @@
-const arr = [10,11,5,7,82];
+const arr = [10,11,5,7,82];   
+
+//Mutatating Methods :- 
+
 arr.push(31);                             //Add Element last in array 
 arr.pop();                                 //Remove last elemnt in array  
 arr.unshift(26);                           //Add Elemnt in starting  
-arr.shift();                               //Remove Element from starting                              
+arr.shift();                               //Remove Element from starting    
+arr.reverse();                           //Reverse the Array   
+arr.sort((a,b)=>a-b);                    //Sort the Array                                         
 console.log(arr);
