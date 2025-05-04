2087715cdf5780fbef3fda701d8b53e55bb9ad79
Ravikumar Jha <jharavi153@gmail.com>
2025-05-04 11:29:07 +0530

    Add String Method

--- Practice.js
+++ Practice.js
@@ -1,8 +1,13 @@
-const get = async()=>{
-  const p =new Promise((resolve, reject)=>{
-    resolve(31);
-  })
-  return p;
+function search(arr){
+  for(let i=0; i<arr.length; i++){
+    for(j=0; j<arr.length-i-1; j++){
+      if(arr[j]> arr[j+1]){
+        [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+      }
+    }
+  }
+ 
+  return arr;
 }
 
-console.log(get());
+console.log(search([43,278,30,31,10]))
---
+++ String.js
@@ -0,0 +1,20 @@
+let str = "RaviArt";
+console.log(str.charAt(4));
+console.log(str.split())
+
+console.log(str.includes("Ar"));
+
+console.log(str.indexOf("A"));
+
+console.log(str.toUpperCase());
+console.log(str.repeat(3));
+
+console.log(str.substring(2,5));
+
+
+let str2 = "Ravikumar" 
+console.log(str2.slice(4)+str2.slice(0,4))
+
+let str3 = "Javascript";
+
+console.log(str3.split("").reverse().join(""));
